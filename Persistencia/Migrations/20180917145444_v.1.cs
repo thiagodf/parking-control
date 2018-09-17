627bb2dcc336675d445e6098c5eb8b99b6efb0ec
Thiago Martins <thiago@p21sistemas.com.br>
2018-09-17 13:50:42 -0300

    commit inicial

---
+++ Persistencia/Migrations/20180917145444_v.1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistencia.Migrations
+{
+    public partial class v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Empresas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Excluido = table.Column<bool>(nullable: false),
+                    Nome = table.Column<string>(maxLength: 150, nullable: false),
+                    CNPJ = table.Column<string>(maxLength: 14, nullable: false),
+                    Responsavel = table.Column<string>(maxLength: 150, nullable: true),
+                    Email = table.Column<string>(maxLength: 150, nullable: true),
+                    Telefone = table.Column<string>(maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empresas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FormasRecebimento",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Excluido = table.Column<bool>(nullable: false),
+                    Descricao = table.Column<string>(maxLength: 150, nullable: false),
+                    Taxa = table.Column<decimal>(nullable: false),
+                    QtdDiasParaReceber = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormasRecebimento", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Excluido = table.Column<bool>(nullable: false),
+                    Nome = table.Column<string>(maxLength: 150, nullable: false),
+                    Login = table.Column<string>(maxLength: 100, nullable: false),
+                    Senha = table.Column<string>(maxLength: 300, nullable: true),
+                    Email = table.Column<string>(maxLength: 150, nullable: true),
+                    Perfil = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Excluido = table.Column<bool>(nullable: false),
+                    UsuarioId = table.Column<int>(nullable: false),
+                    EmpresaId = table.Column<int>(nullable: false),
+                    Telefone = table.Column<string>(maxLength: 20, nullable: true),
+                    CPF = table.Column<string>(maxLength: 11, nullable: false),
+                    Mensalista = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clientes_Empresas_EmpresaId",
+                        column: x => x.EmpresaId,
+                        principalTable: "Empresas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Clientes_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Recebimentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Excluido = table.Column<bool>(nullable: false),
+                    ClienteId = table.Column<int>(nullable: false),
+                    Valor = table.Column<decimal>(nullable: false),
+                    FormaRecebimentoId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recebimentos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Recebimentos_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Recebimentos_FormasRecebimento_FormaRecebimentoId",
+                        column: x => x.FormaRecebimentoId,
+                        principalTable: "FormasRecebimento",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Veiculos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Excluido = table.Column<bool>(nullable: false),
+                    ClienteId = table.Column<int>(nullable: false),
+                    Placa = table.Column<string>(maxLength: 150, nullable: false),
+                    Marca = table.Column<string>(maxLength: 50, nullable: true),
+                    Modelo = table.Column<string>(maxLength: 100, nullable: true),
+                    Cor = table.Column<string>(maxLength: 100, nullable: true),
+                    Observacao = table.Column<string>(maxLength: 999, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veiculos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Veiculos_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movimentacoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    EntrouEm = table.Column<DateTime>(nullable: false),
+                    SaiuEm = table.Column<DateTime>(nullable: true),
+                    VeiculoId = table.Column<int>(nullable: false),
+                    Valor = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movimentacoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Movimentacoes_Veiculos_VeiculoId",
+                        column: x => x.VeiculoId,
+                        principalTable: "Veiculos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clientes_EmpresaId",
+                table: "Clientes",
+                column: "EmpresaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clientes_UsuarioId",
+                table: "Clientes",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movimentacoes_VeiculoId",
+                table: "Movimentacoes",
+                column: "VeiculoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recebimentos_ClienteId",
+                table: "Recebimentos",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recebimentos_FormaRecebimentoId",
+                table: "Recebimentos",
+                column: "FormaRecebimentoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Veiculos_ClienteId",
+                table: "Veiculos",
+                column: "ClienteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movimentacoes");
+
+            migrationBuilder.DropTable(
+                name: "Recebimentos");
+
+            migrationBuilder.DropTable(
+                name: "Veiculos");
+
+            migrationBuilder.DropTable(
+                name: "FormasRecebimento");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Empresas");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
